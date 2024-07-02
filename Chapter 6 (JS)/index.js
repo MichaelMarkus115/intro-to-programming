feea69838435f4fd27a776a9b66a65e2cb902cac
Michael van den Berg <168528596+MichaelMarkus115@users.noreply.github.com>
2024-07-02 15:42:38 +0200

    2nd commit

---
+++ Chapter 6 (JS)/index.js
@@ -0,0 +1,88 @@
+// let message = "Hello world! This is a test message.";
+// console.log(message);
+
+// let myName = "Michael";
+// console.log("Hello, " + myName);
+
+// if (myName.length > 7) {
+//     console.log("Wow, you have a really long name!");
+// } else {
+//     console.log("Your name is not very long.")
+// }
+
+// let lemonChicken = false;
+// let beefWithBlackBeans = false;
+// let sweetAndSourPork = true;
+
+// if (lemonChicken) {
+//     console.log("I'll be having the Lemon Chicken");
+// } else if (beefWithBlackBeans) {
+//     console.log("I'll be having the Beef with Black Beans");
+// } else if (sweetAndSourPork) {
+//     console.log("I'll be having the Sweet and Sour Pork");
+// } else {
+//     console.log("Guess I will be having the rice then!");
+// }
+
+// myName = "Michael";
+// GivenName = prompt("Please enter your desired name: ");
+
+// if (myName === GivenName) {
+//     console.log("Hello me!");
+// } else {
+//     console.log("Hello stranger");
+// }
+
+// dad = "Daniel";
+// GivenName2 = prompt("Please enter a name: ");
+
+// if (dad === GivenName2) {
+//     console.log("Hi dad!");
+// } else {
+//     console.log("Hi stranger");
+// }
+
+// while (sheepcounter < 10) {
+//     console.log("I have counted " + sheepcounter + " sheep!");
+//     sheepcounter++;
+// }
+// console.log("ZzzzzzZZzzzZZZZzzzZZ");
+
+// for (let sheepcounter = 0; sheepcounter < 10; sheepcounter++) {
+//     console.log("I have counted " + sheepcounter + " sheep!");
+// }
+
+// console.log("Zzzzzzzzzzz");
+
+// let helloCount = 3;
+
+// for (i = 0; i < helloCount; i++) {
+//     console.log("Hello!");
+// }
+
+// let animals = ["Lion", "Flamingo", "Polar Bear", "Boa Constrictor"];
+
+// for (i = 0; i < animals.length; i++) {
+//     console.log("This zoo has a " + animals[i] + " in it");
+// }
+
+// let myName = "Michael";
+
+// for (let i = 0; i < myName.length; i++) {
+//     console.log("My name contains a/an: " + myName[i]);
+// }
+
+// for (let x = 2; x < 10000; x = x * 2) {
+//     console.log("The number is: " + x);
+// }
+
+for (let x = 3; x < 10000; x *= 3) {
+    console.log(x);
+}
+
+let x = 3;
+
+while (x < 10000) {
+    console.log(x);
+    x *= 3;
+}
