e20e64b8810b89e2948148295654476b580ea5c0
Michael van den Berg <168528596+MichaelMarkus115@users.noreply.github.com>
2024-07-12 13:36:34 +0200

    7th commit
    Contains Chapters 10-18

---
+++ B2 Chapter 11/index.js
@@ -0,0 +1,28 @@
+"use strict";
+
+let people = [
+  "Cellar",
+  "Flavor",
+  "Cougar",
+  "Chapter",
+  "Mayor",
+  "Anger",
+  "Senator",
+  "Passenger",
+  "Major",
+  "Popular",
+  "Tractor",
+  "Thunder",
+  "Pillar",
+  "Border",
+  "Calendar",
+  "Quarter",
+  "Lunar",
+  "Proper",
+  "Elevator",
+  "Bitter",
+];
+
+people = people.sort();
+
+document.getElementById("peopleIKnow").innerHTML = people.valueOf();
